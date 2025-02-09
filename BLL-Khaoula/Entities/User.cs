9338a5ed44c4132120201a7210964a4ed2baa2d6
Khaoula Ayari <Khawla.ayyari@gmail.com>
2025-02-09 07:24:46 +0100

    User-BLL

--- BLL-Khaoula/Entities/User.cs
+++ BLL-Khaoula/Entities/User.cs
@@ -8,5 +8,31 @@ namespace BLL_Khaoula.Entities
 {
     public class User
     {
+        public Guid User_Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+        private DateTime? _disabledAt;
+
+        public User(Guid user_Id, string first_Name, string last_Name, string email, string password, DateTime createdAt, DateTime? disabledAt)
+        {
+            User_Id = user_Id;
+            First_Name = first_Name;
+            Last_Name = last_Name;
+            Email = email;
+            Password = password;
+            CreatedAt = createdAt;
+            _disabledAt = disabledAt;
+        }
+
+        public bool IsDisabledAt
+        {
+            get {return _disabledAt is not null; }
+        }
+
+        
+
     }
 }
