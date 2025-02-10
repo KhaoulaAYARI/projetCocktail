1a5c545929721f9a52bad776926966faea28e675
Khaoula Ayari <Khawla.ayyari@gmail.com>
2025-02-10 06:14:36 +0100

    BLL: Get()+Get(id)+Insert()

---
+++ BLL-Khaoula/Mappers/Mapper.cs
@@ -0,0 +1,46 @@
+﻿using BLL_Khaoula.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL_Khaoula.Entities;
+using D = DAL_Khaoula.Entities;
+
+namespace BLL_Khaoula.Mappers
+{
+    internal static class Mapper
+    {
+        public static User ToBLL(this D.User user)
+        {
+            if (user is null)
+            {
+                throw new ArgumentNullException("user");
+            }
+                return new BLL_Khaoula.Entities.User (
+                    user.User_Id, 
+                    user.First_Name, 
+                    user.Last_Name, 
+                    user.Email, 
+                    user.Password, 
+                    user.CreatedAt, 
+                    user.DisabledAt);
+            
+        }
+
+        public static D.User ToDAL(this User user) 
+        {
+            return new D.User()
+            {
+                User_Id = user.User_Id,
+                First_Name = user.First_Name,
+                Last_Name = user.Last_Name,
+                Email = user.Email,
+                Password = user.Password,
+                CreatedAt = user.CreatedAt,
+                DisabledAt = (user.IsDisabledAt) ? new DateTime() : null
+
+            };
+        }
+    }
+}
---
+++ BLL-Khaoula/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using BLL_Khaoula.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL_Khaoula.Mappers;
+using D=DAL_Khaoula.Entities;
+
+namespace BLL_Khaoula.Services
+{
+    public class UserService
+    {
+        private DAL_Khaoula.Srvices.UserService _service;
+        public UserService()
+        {
+            _service=new DAL_Khaoula.Srvices.UserService();
+        }
+
+        public IEnumerable<User> Get()
+        {
+            return _service.Get().Select(dal=>dal.ToBLL());
+        }
+        public User Get(Guid id) 
+        {
+            return _service.GetById(id).ToBLL();
+        }
+        public D.User Insert(User user)
+        {
+           return _service.Insert(user.ToDAL());
+        }
+        public void Delete(Guid id)
+        {
+            _service.Delete(id);
+        }
+    }
+}
