8b17ed2f95b312baafcffe972e46dde89b8c00d7
Khaoula Ayari <Khawla.ayyari@gmail.com>
2025-02-14 05:11:07 +0100

    Index+Details ok

--- ASP_MVC/Controllers/UserController.cs
+++ ASP_MVC/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using ASP_MVC.Models.User;
+﻿using ASP_MVC.Mappers;
+using ASP_MVC.Models.User;
 using BLL_Khaoula.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -16,18 +17,28 @@ namespace ASP_MVC.Controllers
         // GET: UserController
         public ActionResult Index()
         {
-            IEnumerable<UserListItem> model = _userService.Get();//je dois crée le Mapper
+            try
+            {
+            IEnumerable<UserListItem> model = _userService.Get().Select(bll=>bll.ToListItem());
+            return View(model);
+            }
+            catch (Exception ex)
+            {
+
+                return RedirectToAction("Error","Home");
+            }
             /*
              Video 29 Janvier 01:17:51 
              jusqu'à 32:51
             */
-            return View(model);
         }
 
         // GET: UserController/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(Guid id)
         {
-            return View();
+
+            UserDetails model = _userService.GetById(id).ToDetails();
+                return View(model);
         }
 
         // GET: UserController/Create
