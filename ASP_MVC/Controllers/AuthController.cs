4c70a90c6c7df98a7f751a58ced24fb727df8aec
KhaoulaAYARI <khawla.ayyari@gmail.com>
2025-02-19 15:08:03 +0100

    AuthController

---
+++ ASP_MVC/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using ASP_MVC.Models.Auth;
+using BLL_Khaoula.Entities;
+using Commun.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        private IUserRepository<BLL_Khaoula.Entities.User> _userService;
+
+        public AuthController(IUserRepository<User> userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction(nameof(Login));
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login (AuthLoginForm form)
+        {
+            try
+            {
+                if (!ModelState.IsValid) throw new ArgumentException(nameof(form));
+                Guid id = _userService.CheckPassword(form.Email,form.Password);
+                return RedirectToAction("Details","User", new {id=id});
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ ASP_MVC/Models/Auth/AuthLoginForm.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ASP_MVC.Models.Auth
+{
+    public class AuthLoginForm
+    {
+        [DisplayName("Email")]
+        [Required(ErrorMessage = "Le champ est obligatoire!")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DisplayName("Mot de passe")]
+        [Required(ErrorMessage = "Le champ est obligatoire!")]
+        [MaxLength(32, ErrorMessage = "au max 64 caractere")]
+        [MinLength(8, ErrorMessage = "au max 8 caractere")]
+        [DataType(DataType.Password)]
+        //[RegularExpression(@"")]
+        public string Password { get; set; }
+
+    }
+}
